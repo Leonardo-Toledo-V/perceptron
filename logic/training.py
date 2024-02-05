80525370a2188a93070f948381fa33a846fe19fb
Leonardo-Toledo-V <leo.toledo.grams@gmail.com>
2024-02-04 21:05:50 -0600

    perceptron

--- logic/training.py
+++ logic/training.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import tkinter as tk
 from logic.graphics import graphic
@@ -8,23 +7,20 @@ class Perceptron:
         self.eta = eta
         self.weight = weight
         self.X = X
-        self.Y = Y
+        self.Y = Y    
+    def functionStep(x):
+        return 1 if x >= 0 else 0
 
 weights = []
 err=[]
 
-def functionStep(x):
-    return 1 if x >= 0 else 0
-
 def initialization(data):
-    column = data.csv_read.shape[1]
-    Y = data.csv_read.iloc[:, column-1]
-    X = data.csv_read.iloc[:,0: column-1]
+    num_columns = data.csv_read.shape[1]
+    Y = data.csv_read.iloc[:, -1].to_numpy().reshape(-1, 1)
+    X = data.csv_read.iloc[:,0: num_columns-1]
     X.insert(0, "x0", 1)
-    weights = [round(w, 2) for w in np.random.uniform(-1, 1, column)]
-    Y = Y.to_numpy().reshape(-1, 1)
     X = X.to_numpy()
-    weights = np.array(weights).reshape(1, -1)
+    weights = np.array( [round(w, 2) for w in np.random.uniform(-1, 1, num_columns)]).reshape(1, -1)
     neuron = Perceptron(data.eta,weights, X, Y)
     cycle(neuron, data.epoch)
 
@@ -36,11 +32,11 @@ def cycle(neuron, epoch):
     weights.clear()
 
 def training(neuron):
-    U = np.linalg.multi_dot([neuron.X, neuron.weight.T])
-    y_C = np.vectorize(functionStep)(U)
+    U = np.dot(neuron.X, neuron.weight.T)
+    y_C = np.vectorize(Perceptron.functionStep)(U)
     error = neuron.Y - y_C
-    change_weights = neuron.eta * np.dot(error.T, neuron.X)
-    neuron.weight = neuron.weight + change_weights
-    norma_error = np.linalg.norm(error)    
-    weights.append(np.squeeze(neuron.weight))
-    err.append(norma_error)
+    newWeights = neuron.eta * np.dot(error.T, neuron.X)
+    neuron.weight = neuron.weight + newWeights
+    normaError = np.linalg.norm(error)    
+    weights.append(neuron.weight.flatten().tolist())
+    err.append(normaError)
